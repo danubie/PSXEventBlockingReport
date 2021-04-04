2311230b8f62c35b1e323ef1783d97ea8ff7c109
Wolfgang Wagner <wolfgang.wagner@inhouse.wko.at>
2021-04-04 12:45:18 +0200

    @linux path names are case sensitiv

--- src/PSXEventBlockingReport.psm1
+++ src/PSXEventBlockingReport.psm1
@@ -1,5 +1,5 @@
-$private = Get-ChildItem (Join-Path $PSScriptRoot "Private") -Recurse -Filter '*.ps1'
-$public = Get-ChildItem (Join-Path $PSScriptRoot "Public") -Recurse -Filter '*.ps1'
+$private = Get-ChildItem (Join-Path $PSScriptRoot "private") -Recurse -Filter '*.ps1'
+$public = Get-ChildItem (Join-Path $PSScriptRoot "public") -Recurse -Filter '*.ps1'
 foreach ($function in $private) {
     . $function.FullName
     Write-Verbose "private: $($function.FullName)"
